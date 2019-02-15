2866c2879771e5ec3360df16b5bbb011f198d09c
Thalison Wilker <thalison.wilker@gmail.com>
2019-02-15 10:44:12 -0200

    Commit

--- main.js
+++ main.js
@@ -2,26 +2,8 @@ $('table').hide()
 $('button').on('click', event => {
 
     $('button').text('Listando ...')
-    
-    $.ajax({
-        url: 'http://jsonplaceholder.typicode.com/users',
-        method: 'GET',
-        dataType: 'jsonp'
-    }).done( users => {
-        let tbody = $('tbody')
-        users.forEach( user =>{
-            let tableRow = `<tr>
-                <td>${user.name}</td>
-                <td>${user.username}</td>
-                <td>${user.email}</td>
-                <td>${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}</td>
-                </tr>
-            `
-            tbody.append(tableRow)
-        })
-    })
-    .always( () => {
-        $('table').show('fast')
-        $('button').text('Listar Novamente')
-    })
+
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => console.log(json))
 })
