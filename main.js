59c1b41f562216c95fcc0cdc2abf026920ae8e8a
Thalison Wilker <thalison.wilker@gmail.com>
2019-02-15 10:46:45 -0200

    Commit

--- main.js
+++ main.js
@@ -4,6 +4,20 @@ $('button').on('click', event => {
     $('button').text('Listando ...')
 
     fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(json => console.log(json))
+    .then(response => response.json())
+    .then(json => {
+        let tbody = $('tbody')
+        json.forEach(user => {
+            let tableRow = `<tr>
+                <td>${user.name}</td>
+                <td>${user.username}</td>
+                <td>${user.email}</td>
+                <td>${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}</td>
+                </tr>
+            `
+            tbody.append(tableRow)
+        })
+        $('table').show('fast')
+        $('button').text('Listar Novamente')
+    })
 })
