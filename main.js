643c9877c771dd75545516575d96738a12235f0a
Thalison Wilker <thalison.wilker@gmail.com>
2019-02-15 10:31:20 -0200

    commit

---
+++ main.js
@@ -0,0 +1,26 @@
+$('table').hide()
+$('button').on('click', event => {
+
+    $('button').text('Listando ...')
+    
+    $.ajax({
+        url: 'http://jsonplaceholder.typicode.com/users',
+        method: 'GET'
+    }).done( users => {
+        let tbody = $('tbody')
+        users.forEach( user =>{
+            let tableRow = `<tr>
+                <td>${user.name}</td>
+                <td>${user.username}</td>
+                <td>${user.email}</td>
+                <td>${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}</td>
+                </tr>
+            `
+            tbody.append(tableRow)
+        })
+    })
+    .always( () => {
+        $('table').show('fast')
+        $('button').text('Listar Novamente')
+    })
+})
