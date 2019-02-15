1d1111152529f9c7e9ba30d22ee03ac04820d753
Thalison Wilker <thalison.wilker@gmail.com>
2019-02-15 10:45:00 -0200

    Commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $('button').on('click', event => {
 
     $('button').text('Listando ...')
 
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
         .then(json => console.log(json))
 })
